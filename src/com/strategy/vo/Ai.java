9cdf302d2ec9e9e1232034e8e1ad1d9d7e943ea1
caolt6907 <2364275179@qq.com>
2019-03-26 14:10:10 +0800

    1

---
+++ src/com/strategy/vo/Ai.java
@@ -0,0 +1,54 @@
+package com.strategy.vo;
+
+public class Ai {
+	private String aiId;
+	private String lyCode;
+	private String type;
+	private String content;
+	private String evaluation;
+	private String nextAIid;
+	private String aicontent;
+	public String getAiId() {
+		return aiId;
+	}
+	public void setAiId(String aiId) {
+		this.aiId = aiId;
+	}
+	public String getLyCode() {
+		return lyCode;
+	}
+	public void setLyCode(String lyCode) {
+		this.lyCode = lyCode;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEvaluation() {
+		return evaluation;
+	}
+	public void setEvaluation(String evaluation) {
+		this.evaluation = evaluation;
+	}
+	public String getNextAIid() {
+		return nextAIid;
+	}
+	public void setNextAIid(String nextAIid) {
+		this.nextAIid = nextAIid;
+	}
+	public String getAicontent() {
+		return aicontent;
+	}
+	public void setAicontent(String aicontent) {
+		this.aicontent = aicontent;
+	}
+
+}
---
+++ src/com/strategy/vo/PostStart.java
@@ -0,0 +1,20 @@
+package com.strategy.vo;
+
+public class PostStart {
+	private String type;
+	private String txt;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTxt() {
+		return txt;
+	}
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+
+}
